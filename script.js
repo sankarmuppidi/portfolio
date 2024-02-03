7cffbbf780b8270a0e2ba04a94ef7774cd4eb920
sankarmuppidi <121499120+sankarmuppidi@users.noreply.github.com>
2024-02-03 10:25:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+  
+  /* ----- TYPING EFFECT ----- */
+  let typingEffect = new Typed(".typedText",{
+    strings : ["data scientist","web Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+  })
+  
+ 
